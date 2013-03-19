004da2425fd66dd7649fc0cf5a98a5c2744ea489
David Steeb <david.steeb@b13.de>
2013-03-19 13:57:38 +0100

    Anpassungen JS für Kompatibilität mit nicht-requireJS-Webseiten

--- res/js/main.js
+++ res/js/main.js
@@ -27,83 +27,89 @@
  * 
  * This package includes the necessary JS functions based on jQuery
  */
+(function($, undefined) {
+
+		// wait until jQuery is defined
+	try {
+		var interval = window.setInterval(function() {
+			var jQueryIsDefined = window.jQuery?true:false;
+			if (jQueryIsDefined) {
+				window.clearInterval(interval);
+				
+				bindTrackingEvents();
+			}
+		}, 50);
+	} catch(err){ console.log(err); }
 
 
-	// wait until jQuery is defined
-try {
-	var interval = window.setInterval(function() {
-		var jQueryIsDefined = window.jQuery?true:false;
-		if (jQueryIsDefined) {
-			window.clearInterval(interval);
-			bindTrackingEvents();
-		}
-	}, 50);
-} catch(err){ console.log(err); }
 
 
+	function bindTrackingEvents() {
 
-function bindTrackingEvents() {
-
-		// tracks external links as events of type "Outbound Links" AND downloads as events of type "Download"
-	$(document).on('click', 'a[data-gabasicstrackexternal="1"], a[data-gabasicstrackdownload="1"]', function(evt) {
-		evt.preventDefault();
-		var 
-			url       = $(this).attr('href')
-			,linkType = '';
-		
-		if ($(this).data('gabasicstrackexternal') == 1) {
-			
-				// events of type "Outbound Links" 
-			linkType = 'Outbound Links';
-			
-		} else if ($(this).data('gabasicstrackdownload') == 1) {
-			
-				// events of type "Download"
-			linkType = 'Download';
-		}
-		
-		try {
-			_gaq.push(['_trackEvent', 'Link', linkType, url]);
-			window.setTimeout(function() {
-				window.open(url);
-			}, 200);
-		} catch(err){ console.log(err); }
-	});
-		
-		// sets gaq.push-link parameter so Google Analytics won't set referers from our own domains
-	$(document).on('click', 'a[data-gabasicstracklink="1"]', function(evt) {
-		evt.preventDefault();
-		
-		var url = $(this).attr('href');
-		
-		try {
-			_gaq.push(['_link', url]);
-			window.setTimeout(function() {
-	//			window.location.href = url;
-	//			window.open(url);
-			}, 200);
-		} catch(err){ console.log(err) }
-	});
+		var $ = window.jQuery;
 	
-		// tracks individually added clicks to Google Analytics as Events
-		// use data-gabasicstrackclick="1" to register for a click and data-gabasicstrackclicklabel="Label" as the label for the event
-	$(document).on('click', 'a[data-gabasicstrackclick="1"]', function(evt) {
-		evt.preventDefault();
-		var 
-			url       = $(this).attr('href')
-			,label    = $(this).data('gabasicstrackclicklabel');
+			// tracks external links as events of type "Outbound Links" AND downloads as events of type "Download"
+		$(document).on('click', 'a[data-gabasicstrackexternal="1"], a[data-gabasicstrackdownload="1"]', function(evt) {
+			evt.preventDefault();
+			var 
+				url       = $(this).attr('href')
+				,linkType = '';
+			
+			if ($(this).data('gabasicstrackexternal') == 1) {
+				
+					// events of type "Outbound Links" 
+				linkType = 'Outbound Links';
+				
+			} else if ($(this).data('gabasicstrackdownload') == 1) {
+				
+					// events of type "Download"
+				linkType = 'Download';
+			}
+			
+			try {
+				_gaq.push(['_trackEvent', 'Link', linkType, url]);
+				window.setTimeout(function() {
+					window.open(url);
+				}, 200);
+			} catch(err){ console.log(err); }
+		});
+			
+			// sets gaq.push-link parameter so Google Analytics won't set referers from our own domains
+		$(document).on('click', 'a[data-gabasicstracklink="1"]', function(evt) {
+			evt.preventDefault();
+			
+			var url = $(this).attr('href');
+			
+			try {
+				_gaq.push(['_link', url]);
+				window.setTimeout(function() {
+		//			window.location.href = url;
+		//			window.open(url);
+				}, 200);
+			} catch(err){ console.log(err) }
+		});
 		
-		if (label.length == 0) {
-			label = "url";
-		}
-		
-		try {
-			_gaq.push(['_trackEvent', 'Click', label, url]);
-			window.setTimeout(function() {
-				window.open(url);
-			}, 200);
-		} catch(err){ console.log(err); }
-		
-	});
+			// tracks individually added clicks to Google Analytics as Events
+			// use data-gabasictrackclick="1" to register for a click and data-gabasictrackclicklabel="Label" as the label for the event
+		$(document).on('click', 'a[data-gabasictrackclick="1"]', function(evt) {
+			evt.preventDefault();
+			var 
+				url       = $(this).attr('href')
+				,label    = $(this).data('gabasictrackclicklabel');
+			
+			if (label.length == 0) {
+				label = "url";
+			}
+			
+			try {
+				_gaq.push(['_trackEvent', 'Click', label, url]);
+				window.setTimeout(function() {
+					window.open(url);
+				}, 200);
+			} catch(err){ console.log(err); }
+			
+		});
+	
+	}
 
-}
+})(window.jQuery)
